5de699986a1e702dc876754c726015fdd3bc0724
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-03-14 20:49:50 -0500

    Editing MySQL config for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 const mysql = require("mysql");
+let connection;
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "p@$$w0rd",
-  database: "eat_da_burger_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "p@$$w0rd",
+    database: "eat_da_burger_db"
+  });
+}
 
 connection.connect(err => {
   if (err) {
